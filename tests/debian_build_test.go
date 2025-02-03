6c46747fdd8a9939ccdff48c4fa726935cf70c3f
antyung <antyung@outlook.sg>
2025-02-03 14:57:43 +0800

    Add files

---
+++ tests/debian_build_test.go
@@ -0,0 +1,64 @@
+package tests
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/testcontainers/testcontainers-go"
+)
+
+var DebianBuild = struct {
+	DOCKER_IMAGE       string
+	DOCKER_IMAGE_TAG   string
+	AWS_ECR_URI        string
+	DOCKER_IMAGE_GROUP string
+}{
+	DOCKER_IMAGE:       "debian-build",
+	DOCKER_IMAGE_TAG:   "latest",
+	AWS_ECR_URI:        "public.ecr.aws/w2u0w5i6",
+	DOCKER_IMAGE_GROUP: "ci",
+}
+
+func TestContainerBuildDebianBuild(t *testing.T) {
+	ctx := context.Background()
+	container, e := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			FromDockerfile: testcontainers.FromDockerfile{
+				Context:       "../" + DebianBuild.DOCKER_IMAGE + "/",
+				Dockerfile:    "Dockerfile.debian",
+				KeepImage:     false,
+				PrintBuildLog: true,
+			},
+		},
+		Started: true,
+	})
+	require.NoError(t, e)
+	testcontainers.CleanupContainer(t, container)
+}
+
+func TestContainerPullDebianBuild(t *testing.T) {
+	ctx := context.Background()
+	container, e := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: DebianBuild.AWS_ECR_URI + "/" + DebianBuild.DOCKER_IMAGE_GROUP + "/" + DebianBuild.DOCKER_IMAGE + ":" + DebianBuild.DOCKER_IMAGE_TAG,
+		},
+		Started: false,
+	})
+	require.NoError(t, e)
+	testcontainers.CleanupContainer(t, container)
+}
+
+func TestContainerExecDebianBuild(t *testing.T) {
+	ctx := context.Background()
+	container, e := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: DebianBuild.AWS_ECR_URI + "/" + DebianBuild.DOCKER_IMAGE_GROUP + "/" + DebianBuild.DOCKER_IMAGE + ":" + DebianBuild.DOCKER_IMAGE_TAG,
+			Cmd:   []string{"gcc", "--version"},
+		},
+		Started: true,
+	})
+	require.NoError(t, e)
+	testcontainers.CleanupContainer(t, container)
+}
